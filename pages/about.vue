0cff4f7d636bb41c2e69eb50c1b0dda86684b4e5
Jaderiel <jabuela22@gmail.com>
2024-03-05 14:29:49 +0800

    about and home page

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione similique corporis exercitationem quas distinctio minima ab cum ipsum, dolorem fugit!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam debitis praesentium aliquam enim! Sapiente alias impedit culpa mollitia? Vitae, ratione.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
